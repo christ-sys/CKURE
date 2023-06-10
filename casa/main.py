1983944c94fb136590efe4849454ead9b4a8c975
Ichan <christremigio6@gmail.com>
2023-06-10 21:05:13 +0800

    fetch image on db
    dialog not working yet

--- casa/main.py
+++ casa/main.py
@@ -1,3 +1,4 @@
+from sqlite3 import dbapi2
 import sys
 import os
 import pytz
@@ -11,6 +12,7 @@ from kivymd.uix.list import *
 from kivymd.uix.button import *
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.menu import MDDropdownMenu
+from kivymd.uix.imagelist import *
 
 from kivy.uix.boxlayout import BoxLayout
 from kivy.clock import Clock
@@ -60,26 +62,23 @@ class ReportDetails(Screen):
         super(ReportDetails, self).__init__(**kwargs)
         self.report_data = {}
         self.report_id = None
+    
 
     def report_details(self, report_id):
         self.report_id = report_id
         self.report_data = firestoredb.get_report_details(report_id)
         self.report_labels()
+        self.getImages(report_id)
     def report_labels(self):
         ph_tz = pytz.timezone('Asia/Manila')
         name_label = self.ids.name_label
         time_label = self.ids.time_label
-        title = self.ids.title
-        title.title = self.report_data.get('Name', '')
-        name_label.secondary_text = self.report_data.get('Name', '')
+        # title = self.ids.title
+        # title.title = self.report_data.get('Name', '')
+        name_label.secondary_text = self.report_data.get('Driver_Name', '')
         time_label.secondary_text = self.report_data.get('Time', '')
     
-    def estimated_cost(self,labor, replace):
-        sum = int(labor) + int(replace)
-        return sum
-        
-
-    def show_confirmation_dialog(self):
+    def show_confirmation_dialog(self, *args):
         if not self.dialog:
             self.dialog = MDDialog(
                 title="Estimations",
@@ -100,6 +99,45 @@ class ReportDetails(Screen):
                 ],
             )
         self.dialog.open()
+    
+    def getImages(self,id):
+        container = self.ids.SmartTilecontainer
+        container.clear_widgets()
+        
+        # mytile.add_widget(listitem)
+        myrecord = firestoredb.db.collection('reports').document(id).get()
+        myrecord =myrecord.to_dict()
+        for img,panel,cost in zip(myrecord['ImgRef'],myrecord['PanelsPart'],myrecord['Costs']):
+            mytile = MDSmartTile(
+            radius=24,
+            source=img,
+            box_radius = [0, 0, 24, 24],
+            box_color = [1, 1, 1, .2],
+            pos_hint = {"center_x": .5, "center_y": .5},
+            size_hint = (1,None),
+            size = ("150dp", "150dp"),
+            on_press = self.show_confirmation_dialog
+        )
+
+            mytile.add_widget(TwoLineListItem(
+            
+            text=panel,
+            secondary_text = cost,
+            pos_hint= {"center_y": .5},
+            _no_ripple_effect = True,
+            text_color = '#ffffff',
+            secondary_text_color = '#808080'
+            ))
+            
+            # mytile.bind()
+            container.add_widget(mytile)
+
+    def estimated_cost(self,labor, replace):
+        sum = int(labor) + int(replace)
+        return sum
+        
+
+    
 
     def ok_dialog(self, *args):
         # self.dialog.dismiss()
@@ -132,15 +170,15 @@ class PnpReports(Screen):
         approved_list.clear_widgets()
         ph_tz = pytz.timezone('Asia/Manila')
         for report in reports:
-            name_text = "Name: " + str(report['Name'])
-            report_id_text = "Report ID: " + str(report['id'])
+            name_text = "Name: " + str(report['Driver_Name'])
+            report_id_text = "Report ID: " + str(report['report_sender'])
             date_text = "Time: " + str(report['Time'])
-            if report['Status'] == "Pending":
+            if report['status'] == "Pending":
                 item = ThreeLineRightIconListItem(text=name_text, secondary_text=report_id_text, tertiary_text=date_text)
                 item.add_widget(IconRightWidget(icon="chevron-right"))
                 item.bind(on_release=lambda x, report_id=report['id']: self.on_select(report_id))
                 reports_list.add_widget(item)
-            elif report['Status'] == "Approved":
+            elif report['status'] == "Approved":
                 item = ThreeLineRightIconListItem(text=name_text, secondary_text=report_id_text, tertiary_text=date_text)
                 approved_list.add_widget(item)
 
